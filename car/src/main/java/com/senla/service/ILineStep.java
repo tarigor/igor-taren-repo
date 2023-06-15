fbcd79cd94811ccb2ce807faabe986a4842e4794
Igor Taren <tarigor80@gmail.com>
2023-06-15 09:11:14 +0200

    project skeleton created

--- car/src/main/java/com/senla/service/ILineStep.java
+++ car/src/main/java/com/senla/service/ILineStep.java
@@ -1,5 +1,5 @@
 package com.senla.service;
 
 public interface ILineStep {
-    IProductPart buildProductPart();
+    IProductPart buildProductPart(String... partName);
 }
--- car/src/main/java/com/senla/service/impl/LineStep.java
+++ car/src/main/java/com/senla/service/impl/LineStep.java
@@ -4,8 +4,9 @@ import com.senla.service.ILineStep;
 import com.senla.service.IProductPart;
 
 public class LineStep implements ILineStep {
+
     @Override
-    public IProductPart buildProductPart(String firstPartName,String secondPartName,String thirdPartName) {
-        return new ProductPart();
+    public IProductPart buildProductPart(String... partNames) {
+        return null;
     }
 }
