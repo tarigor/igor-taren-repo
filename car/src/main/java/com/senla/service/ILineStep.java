86b39d62b0a0eb7aa2e4553b54e6bf1f90ac174b
Igor Taren <tarigor80@gmail.com>
2023-06-17 12:07:09 +0200

    project completed

--- car/src/main/java/com/senla/service/ILineStep.java
+++ car/src/main/java/com/senla/service/ILineStep.java
@@ -1,5 +1,7 @@
 package com.senla.service;
 
+import com.senla.entity.ProductPart;
+
 public interface ILineStep {
-    IProductPart buildProductPart(String... partName);
+    ProductPart buildProductPart();
 }
