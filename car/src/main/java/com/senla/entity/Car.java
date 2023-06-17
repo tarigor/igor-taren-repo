e32f433fce8715b71978a7b3de327bb431d20902
Igor Taren <tarigor80@gmail.com>
2023-06-17 12:13:13 +0200

    maven deleted

--- car/src/main/java/com/senla/entity/Car.java
+++ car/src/main/java/com/senla/entity/Car.java
@@ -1,14 +1,28 @@
 package com.senla.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Car {
     private ProductPart bodyPart;
     private ProductPart chassisPart;
     private ProductPart enginePart;
+
+    public void setBodyPart(ProductPart bodyPart) {
+        this.bodyPart = bodyPart;
+    }
+
+    public void setChassisPart(ProductPart chassisPart) {
+        this.chassisPart = chassisPart;
+    }
+
+    public void setEnginePart(ProductPart enginePart) {
+        this.enginePart = enginePart;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "bodyPart=" + bodyPart +
+                ", chassisPart=" + chassisPart +
+                ", enginePart=" + enginePart +
+                '}';
+    }
 }
