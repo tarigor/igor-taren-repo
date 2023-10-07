75cc7da55ddd293e749ac93965a9aab22fd696e1
Igor Taren <tarigor80@gmail.com>
2023-10-07 09:21:44 +0300

    logic for accessing to DB integrated in DAO classes

--- hotel-serialization/src/main/java/com/serialization/SerializationService.java
+++ hotel-serialization/src/main/java/com/serialization/SerializationService.java
@@ -5,7 +5,7 @@ import com.google.gson.GsonBuilder;
 import com.senla.container.CreateInstanceAndPutInContainer;
 import com.senla.container.InjectValue;
 import com.senla.hotel.entity.*;
-import com.senla.hotel.repo.impl.*;
+import com.senla.hotel.service.impl.*;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -16,61 +16,61 @@ import java.util.stream.Collectors;
 @CreateInstanceAndPutInContainer
 public class SerializationService {
     private static final String RESOURCES_PATH = "\\hotel-serialization\\resources\\serialized";
-    private BookingRepositoryImpl bookingRepository;
-    private GuestRepositoryImpl guestRepository;
-    private GuestServiceRepositoryImpl guestServiceRepository;
-    private RoomRepositoryImpl roomRepository;
-    private RoomServiceRepositoryImpl roomServiceRepository;
+    private BookingServiceImpl bookingService;
+    private GuestServiceImpl guestService;
+    private GuestServicesServiceImpl guestServicesService;
+    private RoomServiceImpl roomService;
+    RoomServicesServiceImpl roomServicesService;
 
-    @InjectValue(key = "BookingRepositoryImpl")
-    public void setBookingRepository(BookingRepositoryImpl bookingRepository) {
-        this.bookingRepository = bookingRepository;
+    @InjectValue(key = "BookingServiceImpl")
+    public void setBookingService(BookingServiceImpl bookingService) {
+        this.bookingService = bookingService;
     }
 
-    @InjectValue(key = "GuestRepositoryImpl")
-    public void setGuestRepository(GuestRepositoryImpl guestRepository) {
-        this.guestRepository = guestRepository;
+    @InjectValue(key = "GuestServiceImpl")
+    public void setGuestService(GuestServiceImpl guestService) {
+        this.guestService = guestService;
     }
 
-    @InjectValue(key = "GuestServiceRepositoryImpl")
-    public void setGuestServiceRepository(GuestServiceRepositoryImpl guestServiceRepository) {
-        this.guestServiceRepository = guestServiceRepository;
+    @InjectValue(key = "GuestServicesServiceImpl")
+    public void setGuestServicesService(GuestServicesServiceImpl guestServicesService) {
+        this.guestServicesService = guestServicesService;
     }
 
-    @InjectValue(key = "RoomRepositoryImpl")
-    public void setRoomRepository(RoomRepositoryImpl roomRepository) {
-        this.roomRepository = roomRepository;
+    @InjectValue(key = "RoomServiceImpl")
+    public void setRoomService(RoomServiceImpl roomService) {
+        this.roomService = roomService;
     }
 
-    @InjectValue(key = "RoomServiceRepositoryImpl")
-    public void setRoomServiceRepository(RoomServiceRepositoryImpl roomServiceRepository) {
-        this.roomServiceRepository = roomServiceRepository;
+    @InjectValue(key = "RoomServicesServiceImpl")
+    public void setRoomServicesService(RoomServicesServiceImpl roomServicesService) {
+        this.roomServicesService = roomServicesService;
     }
 
     public void selectToSerialize(String mapName) {
         switch (mapName) {
             case "Booking": {
-                serializeMap(bookingRepository.getAll().stream()
+                serializeMap(bookingService.getAll().stream()
                         .collect(Collectors.groupingBy(Booking::getId)), "Booking");
                 break;
             }
             case "Guest": {
-                serializeMap(guestRepository.getAll().stream()
+                serializeMap(guestService.getAll().stream()
                         .collect(Collectors.groupingBy(Guest::getId)), "Guest");
                 break;
             }
             case "GuestServices": {
-                serializeMap(guestServiceRepository.getAll().stream()
+                serializeMap(guestServicesService.getAll().stream()
                         .collect(Collectors.groupingBy(GuestServices::getId)), "GuestServices");
                 break;
             }
             case "Room": {
-                serializeMap(roomRepository.getAll().stream()
+                serializeMap(roomService.getAll().stream()
                         .collect(Collectors.groupingBy(Room::getId)), "Room");
                 break;
             }
             case "RoomServices": {
-                serializeMap(roomServiceRepository.getAll().stream()
+                serializeMap(roomServicesService.getAll().stream()
                         .collect(Collectors.groupingBy(RoomService::getId)), "RoomServices");
                 break;
             }
--- hotel/src/main/java/com/senla/hotel/dao/impl/RoomServiceDAOImpl.java
+++ hotel/src/main/java/com/senla/hotel/dao/impl/RoomServiceDAOImpl.java
@@ -2,43 +2,139 @@ package com.senla.hotel.dao.impl;
 
 import com.senla.container.CreateInstanceAndPutInContainer;
 import com.senla.container.InjectValue;
+import com.senla.hotel.constant.ServiceType;
 import com.senla.hotel.dao.IEntityDAO;
 import com.senla.hotel.entity.RoomService;
-import com.senla.hotel.repo.impl.RoomServiceRepositoryImpl;
+import com.senla.hoteldb.DatabaseService;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @CreateInstanceAndPutInContainer
 public class RoomServiceDAOImpl implements IEntityDAO<RoomService> {
-    private RoomServiceRepositoryImpl roomServiceRepository;
+    public static final String SELECT_FROM = "SELECT * FROM room_service";
+    public static final String SELECT_WHERE_ID = "SELECT * FROM room_service WHERE id=?";
+    public static final String INSERT_INTO = "INSERT INTO room_service (service_type, price) VALUES (?, ?)";
+    public static final String UPDATE = "UPDATE room_service SET service_type=?, price=? WHERE id=";
+    protected DatabaseService databaseService;
 
-    @InjectValue(key = "RoomServiceRepositoryImpl")
-    public void setRoomServiceRepository(RoomServiceRepositoryImpl roomServiceRepository) {
-        this.roomServiceRepository = roomServiceRepository;
+    @InjectValue(key = "DatabaseService")
+    public void setDatabaseService(DatabaseService databaseService) {
+        this.databaseService = databaseService;
     }
 
     @Override
     public List<RoomService> getAll() {
-        return roomServiceRepository.getAll();
+        Connection connection = databaseService.getConnection();
+        List<RoomService> result = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String query = SELECT_FROM;
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                RoomService item = mapRoomServiceResultSet(resultSet);
+                result.add(item);
+            }
+            resultSet.close();
+            statement.close();
+            if (connection.getAutoCommit()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public RoomService getById(long id) {
+        Connection connection = databaseService.getConnection();
+        RoomService result = null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_WHERE_ID);
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                result = mapRoomServiceResultSet(resultSet);
+            }
+            resultSet.close();
+            preparedStatement.close();
+            if (connection.getAutoCommit()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public void saveAll(List<RoomService> roomServices) {
-        roomServiceRepository.saveAll(roomServices);
+        Connection connection = databaseService.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_INTO);
+            for (RoomService roomService : roomServices) {
+                setRoomServiceValues(preparedStatement, roomService);
+                preparedStatement.executeUpdate();
+            }
+            preparedStatement.close();
+            if (connection.getAutoCommit()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public RoomService update(RoomService roomService) {
-        return roomServiceRepository.update(roomService);
-    }
-
-    @Override
-    public RoomService getById(long serviceId) {
-        return roomServiceRepository.getById(serviceId);
+        Connection connection = databaseService.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE + roomService.getId());
+            setRoomServiceValues(preparedStatement, roomService);
+            int rowsAffected = preparedStatement.executeUpdate();
+            preparedStatement.close();
+            if (connection.getAutoCommit()) {
+                connection.close();
+            }
+            if (rowsAffected > 0) {
+                return roomService;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public void save(RoomService roomService) {
-        roomServiceRepository.save(roomService);
+        Connection connection = databaseService.getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_INTO);
+            setRoomServiceValues(preparedStatement, roomService);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            if (connection.getAutoCommit()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private RoomService mapRoomServiceResultSet(ResultSet resultSet) throws SQLException {
+        RoomService roomService = new RoomService();
+        roomService.setId(resultSet.getLong("id"));
+        roomService.setServiceType(ServiceType.valueOf(resultSet.getString("service_type")));
+        roomService.setPrice(resultSet.getDouble("price"));
+        return roomService;
+    }
+
+    private void setRoomServiceValues(PreparedStatement preparedStatement, RoomService roomService) throws SQLException {
+        preparedStatement.setString(1, roomService.getServiceType().toString());
+        preparedStatement.setDouble(2, roomService.getPrice());
     }
 }
