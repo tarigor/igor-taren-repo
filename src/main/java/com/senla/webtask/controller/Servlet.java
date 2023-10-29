4b9e4286074010f187f4c99cec9c4aca35ea3f19
Igor Taren <tarigor80@gmail.com>
2023-10-29 11:14:21 +0100

    web page improved. navigation added. error input handled

--- src/main/java/com/senla/webtask/controller/Servlet.java
+++ src/main/java/com/senla/webtask/controller/Servlet.java
@@ -7,11 +7,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/hello")
+@WebServlet(urlPatterns = {"/"})
 public class Servlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-        req.setAttribute("message", "Hello World!");
-        req.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(req, resp);
+        String requestURI = req.getRequestURI();
+        if (requestURI.endsWith("/")) {
+            handleRequest("Start Page", "click -> <a href=\"/hello\">/hello</a>", req, resp);
+        } else if (requestURI.endsWith("/hello")) {
+            handleRequest("Hello World Page", "Hello World!", req, resp);
+        }
+        handleRequest("Error Page", "oops...", req, resp);
+    }
+
+    private void handleRequest(String pageName, String message, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("pageName", pageName);
+        req.setAttribute("message", message);
+        req.getRequestDispatcher("/WEB-INF/jsp/message.jsp").forward(req, resp);
     }
 }
