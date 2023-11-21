00fe71a8e02b44a3f96eb0d3b7929c5ef19ebd8a
Igor Taren <tarigor80@gmail.com>
2023-11-21 13:51:18 +0100

    tests StringToDateConverter class refactored

--- hotel/src/test/java/com/senla/hotel/deserializationservice/StringToDateConverterTest.java
+++ hotel/src/test/java/com/senla/hotel/deserializationservice/StringToDateConverterTest.java
@@ -8,6 +8,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @ExtendWith(MockitoExtension.class)
 class StringToDateConverterTest {
@@ -16,13 +18,12 @@ class StringToDateConverterTest {
     private StringToDateConverter stringToDateConverter;
 
     @Test
-    void convert() {
+    void convertDateResultTest() {
         String dateString = "Nov 11, 2023, 01:30:00 PM";
 
         Date result = stringToDateConverter.convert(dateString);
 
-        Assertions.assertNotNull(result, "Parsed date should not be null");
-        Assertions.assertEquals("Sat Nov 11 13:30:00 CET 2023", result.toString(),
+        assertEquals("Sat Nov 11 13:30:00 CET 2023", result.toString(),
                 "Parsed date does not match the expected date");
     }
 }
